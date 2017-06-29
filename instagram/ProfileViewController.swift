41f9e8325ae206628b2fec1644befa15d27d0cc6
Jessica Yeh <jessicayeh@fb.com>
2017-06-28 17:48:50 -0700

    user can click on images in profile view to see detail view

--- instagram/ProfileViewController.swift
+++ instagram/ProfileViewController.swift
@@ -65,14 +65,15 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource {
             }
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let cell = sender as? UICollectionViewCell {
+            if let indexPath = collectionView.indexPath(for: cell) {
+                let post = userPosts[indexPath.row]
+                let detailVC = segue.destination as! DetailViewController
+                detailVC.post = post
+            }
+        }
+        
     }
-    */
-
 }
